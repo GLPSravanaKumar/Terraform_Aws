d1eb5c6c8691f18007026ad6c1161c7c823b368b
Glpskumar <151714675+GLPSravanaKumar@users.noreply.github.com>
2024-11-19 11:29:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,9 @@ terraform {
   }
 }
 provider "aws" {
+shared_config_files      = ["/Users/tf_user/.aws/conf"]
+shared_credentials_files = ["/Users/tf_user/.aws/creds"]
+AWS_PROFILE                 = "customprofile"
 region = "ap-south-1"
 }
 resource "aws_vpc" "tf_vpc" {
